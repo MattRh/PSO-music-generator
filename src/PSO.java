4c033f792278d31c9b66dfe2c0648ee56432e0b5
Sergey <sergejrf@gmail.com>
2017-11-03 16:41:29 +0300

    Problem still remains but this implementation is with ArrayList

--- src/PSO.java
+++ src/PSO.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -9,13 +10,13 @@ public class PSO<PT extends IParticle> {
     private final int POPULATION_SIZE = 16;
     private final int ITERATIONS = 10000;
 
-    private PT[] population;
+    private ArrayList<PT> population;
 
     private PT globalBest;
     private double bestFitness;
 
     PSO() {
-        population = (PT[])new Object[POPULATION_SIZE];
+        population = new ArrayList<>(POPULATION_SIZE);
     }
 
     /**
@@ -25,26 +26,26 @@ public class PSO<PT extends IParticle> {
         generatePopulation();
 
         int rnd = new Random().nextInt(POPULATION_SIZE);
-        globalBest = population[rnd];
+        globalBest = population.get(rnd);
         bestFitness = globalBest.getFitness();
 
         for(int i = 0; i < ITERATIONS; i++) {
             int newBestIndex = -1;
 
             for(int j = 0; j < POPULATION_SIZE; j++) {
-                population[j].updateVelocity(globalBest);
-                population[j].updateParticle();
+                population.get(j).updateVelocity(globalBest);
+                population.get(j).updateParticle();
 
-                if(population[i].getFitness() >= bestFitness) {
+                if(population.get(i).getFitness() >= bestFitness) {
                     newBestIndex = i;
 
                     // Small hack to search now only values that are larger than that
-                    bestFitness = population[i].getFitness();
+                    bestFitness = population.get(i).getFitness();
                 }
             }
 
             if(newBestIndex >= 0) {
-                globalBest = (PT) population[i].clone();
+                globalBest = (PT) population.get(i).clone();
                 bestFitness = globalBest.getFitness();
             }
         }
@@ -54,7 +55,7 @@ public class PSO<PT extends IParticle> {
 
     private void generatePopulation() {
         for(int i = 0; i < POPULATION_SIZE; i++) {
-            population[i] = (PT)new Object();
+            population.set(i, (PT)new Object());
         }
     }
 
